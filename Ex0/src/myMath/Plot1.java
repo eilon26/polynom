b4df35505b4f10ef41798ac0902eaac307101c56
EILON <EILON@EILON-LAPTOP>
2018-11-13 17:26:16 +0200

    git non

--- Ex0/src/myMath/Plot1.java
+++ Ex0/src/myMath/Plot1.java
@@ -1,8 +1,17 @@
 package myMath;
 
+<<<<<<< HEAD
+//import java.awt.Graphics2D;
+//import java.util.ArrayList;
+=======
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+>>>>>>> refs/heads/polynom_add_plot
 import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
+//import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
 
 /**
  * Creates a simple Chart using QuickChart
@@ -10,8 +19,6 @@ import org.knowm.xchart.XYChart;
 public class Plot1 {
  
   public static void main(String[] args) throws Exception {
-	System.out.println("enter polynom: ");
-	
     Polynom pol = new Polynom("0.2*x^4-1.5*x^3+3*x^2-1*x^1-5*x^0");
     
     //calculate the graph points
